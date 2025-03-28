3b759e82ee1534b081602b1052864a50169d4cd7
Alexey Potapov <2furius@gmail.com>
2025-03-28 14:52:28 +0300

    feat: new urls

--- oura_bot/urls.py
+++ oura_bot/urls.py
@@ -2,5 +2,6 @@ API_BASE_URL = 'https://api.ouraring.com/'
 API_VERSION = 'v2/'
 API_ROOT = API_BASE_URL + API_VERSION
 
-TOTAL_SLEEP_URL = f'{API_ROOT}usercollection/daily_sleep'
+TOTAL_DAYLY_SLEEP_URL = f'{API_ROOT}usercollection/daily_sleep'
+TOTAL_SLEEP_URL = f'{API_ROOT}usercollection/sleep'
 DAILY_READINESS = f'{API_ROOT}usercollection/daily_readiness'
